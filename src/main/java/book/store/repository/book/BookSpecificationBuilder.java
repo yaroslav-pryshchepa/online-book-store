955fe92643e961876373643bc0a72cb27095897c
Yaroslav Pryshchepa <yrikpa200022@gmail.com>
2025-05-29 20:56:44 +0300

    added constants

--- src/main/java/book/store/repository/book/BookSpecificationBuilder.java
+++ src/main/java/book/store/repository/book/BookSpecificationBuilder.java
@@ -11,21 +11,24 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
+    private static final String AUTHOR = "author";
+    private static final String TITLE = "title";
+    private static final String ISBN = "isbn";
     private final SpecificationProviderManager<Book> bookSpecificationProviderManager;
 
     @Override
     public Specification<Book> build(BookSearchParametersDto searchParameters) {
         Specification<Book> spec = Specification.where(null);
         if (searchParameters.authors() != null && searchParameters.authors().length > 0) {
-            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("author")
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider(AUTHOR)
                     .getSpecification(searchParameters.authors()));
         }
         if (searchParameters.titles() != null && searchParameters.titles().length > 0) {
-            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("title")
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider(TITLE)
                     .getSpecification(searchParameters.titles()));
         }
         if (searchParameters.isbns() != null && searchParameters.isbns().length > 0) {
-            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("isbn")
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider(ISBN)
                     .getSpecification(searchParameters.isbns()));
         }
         return spec;
