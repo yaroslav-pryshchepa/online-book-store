e60add5b83e04ee8727f0b215ce2062a32c6e0fb
yaroslav <yrikpa200022@gmail.com>
2025-06-22 00:09:09 +0300

    added fixes

--- src/main/java/book/store/dto/user/UserLoginResponseDto.java
+++ src/main/java/book/store/dto/user/UserLoginResponseDto.java
@@ -1,14 +1,12 @@
 package book.store.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserLoginResponseDto {
     @NotBlank
     private String token;
-
-    public UserLoginResponseDto(String token) {
-        this.token = token;
-    }
 }
--- src/main/java/book/store/security/JwtAuthenticationFilter.java
+++ src/main/java/book/store/security/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,6 +19,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @RequiredArgsConstructor
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final String BEARER_TOKEN_NAME = "Bearer ";
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
 
@@ -42,9 +44,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String getToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
+        String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_TOKEN_NAME)) {
+            return bearerToken.substring(BEARER_TOKEN_NAME.length());
         }
         return null;
     }
