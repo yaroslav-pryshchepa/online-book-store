4eb8fa091f4a091f1f4d17e1f8a12105e90a48cd
Yaroslav Pryshchepa <yrikpa200022@gmail.com>
2025-06-24 20:43:05 +0300

    Added fixes

--- src/main/java/book/store/dto/category/CreateCategoryRequestDto.java
+++ src/main/java/book/store/dto/category/CreateCategoryRequestDto.java
@@ -1,11 +1,11 @@
 package book.store.dto.category;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class CreateCategoryRequestDto {
-    @NotBlank
+    @NotEmpty
     private String name;
     private String description;
 }
