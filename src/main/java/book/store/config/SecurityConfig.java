771f92e5c4b4ed6a95bd4fe99b22c034f7b2fe01
Yaroslav Pryshchepa <yrikpa200022@gmail.com>
2025-07-10 21:15:38 +0300

    Edited SecurityConfig №2

--- src/main/java/book/store/config/SecurityConfig.java
+++ src/main/java/book/store/config/SecurityConfig.java
@@ -46,7 +46,6 @@ public class SecurityConfig {
                                 .anyRequest()
                                 .authenticated()
                 )
-                .httpBasic(withDefaults())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .userDetailsService(userDetailsService)
